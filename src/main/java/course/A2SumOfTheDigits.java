4cf7bd70de1d29b263d55a519b888b4ac7585bbd
patrakhin <patrakhin@mail.ru>
2023-02-08 11:22:42 +0300

    add sol at task 2

--- src/main/java/course/A2SumOfTheDigits.java
+++ src/main/java/course/A2SumOfTheDigits.java
@@ -5,26 +5,19 @@ import org.apache.log4j.Logger;
 public class A2SumOfTheDigits {
     private static final Logger logger = Logger.getLogger(A2SumOfTheDigits.class);
     public static int getSum (int inputValue) {
-        String numberInput = String.valueOf(inputValue);
-        Character firstElementNum = numberInput.charAt(0);
-        if (firstElementNum.equals('-')) {
-            numberInput = String.valueOf(inputValue * -1);
+        if (inputValue < 0) {
+            inputValue = inputValue * -1;
         }
-        Character nextElementNum = numberInput.charAt(numberInput.length() - 1);
-        int elementFromNumber = Integer.parseInt(String.valueOf(nextElementNum));
-        char [] constructionString = numberInput.toCharArray();
-        char [] outputConstruction = new char[constructionString.length - 1];
-        int outNumber;
-        if (constructionString.length == 1) {
-            return elementFromNumber;
+        int nextElement = inputValue % 10;
+        int nextNumber = inputValue / 10;
+        if (nextNumber % 10 == 0) {
+            return nextElement;
         }
-        System.arraycopy(constructionString, 0, outputConstruction, 0, constructionString.length - 1);
-        outNumber = Integer.parseInt(String.valueOf(outputConstruction));
-        return elementFromNumber + getSum(outNumber);
+        return nextElement + getSum(nextNumber);
     }
 
     public static void main(String[] args) {
-        int a = -54321; // only for check logger, tests are present
+        int a = 54321; // only for check logger, tests are present
         logger.debug(getSum(a));
     }
 }
