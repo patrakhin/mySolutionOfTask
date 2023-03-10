c53a03bbeeaf93ad767e9d9107f76ed5952b67dd
patrakhin <patrakhin@mail.ru>
2023-03-10 18:06:45 +0300

    findFiles get path and list

--- src/main/java/course/A8FindAllFilesSpecDir.java
+++ src/main/java/course/A8FindAllFilesSpecDir.java
@@ -7,33 +7,25 @@ import java.util.*;
 public class A8FindAllFilesSpecDir {
     private static final Logger logger = Logger.getLogger(A8FindAllFilesSpecDir.class);
 
-    public List<String> findFiles(String path) {
-        List<String> searchResult = new ArrayList<>();
+    public void findFiles(String path, List<String> searchResult) {
         File foundFolder = new File(path);
-        findFilesRecursive(foundFolder, searchResult);
-        return searchResult;
-    }
-
-    private void findFilesRecursive(File folder, List<String> searchResult) {
-        File[] foundFiles = folder.listFiles();
-        if (foundFiles == null) {
-            return;
-        }
+        File[] foundFiles = foundFolder.listFiles();
+        assert foundFiles != null;
         for (File file : foundFiles) {
             if (file.isFile()) {
                 searchResult.add(file.getAbsolutePath());
             } else if (file.isDirectory()) {
-                findFilesRecursive(file, searchResult);
+                findFiles(file.getAbsolutePath(), searchResult);
             }
         }
     }
 
     public static void main(String[] args) {
-        A8FindAllFilesSpecDir finder = new A8FindAllFilesSpecDir();
-        List<String> files = finder.findFiles("/home/andrey/Загрузки/muSolutionTask/src/main/java/");
-        for (String file : files) {
-            logger.debug(file);
-        }
+        A8FindAllFilesSpecDir findAllFilesSpecDir = new A8FindAllFilesSpecDir();
+        String path = "/home/andrey/Загрузки/muSolutionTask/src/main/java/";
+        List<String> fileList = new ArrayList<>();
+        findAllFilesSpecDir.findFiles (path, fileList);
+        logger.debug(fileList);
     }
 }
 
