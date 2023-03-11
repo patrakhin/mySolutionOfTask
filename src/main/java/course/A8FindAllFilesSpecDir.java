62b6f3d0ec26551647c851e996b75193a0843eb9
patrakhin <patrakhin@mail.ru>
2023-03-11 19:00:59 +0300

    exchange elise if - searchResult.addAll

--- src/main/java/course/A8FindAllFilesSpecDir.java
+++ src/main/java/course/A8FindAllFilesSpecDir.java
@@ -7,7 +7,8 @@ import java.util.*;
 public class A8FindAllFilesSpecDir {
     private static final Logger logger = Logger.getLogger(A8FindAllFilesSpecDir.class);
 
-    public void findFiles(String path, List<String> searchResult) {
+    public List<String> findFiles(String path) {
+        List<String> searchResult = new ArrayList<>();
         File foundFolder = new File(path);
         File[] foundFiles = foundFolder.listFiles();
         assert foundFiles != null;
@@ -15,16 +16,17 @@ public class A8FindAllFilesSpecDir {
             if (file.isFile()) {
                 searchResult.add(file.getAbsolutePath());
             } else if (file.isDirectory()) {
-                findFiles(file.getAbsolutePath(), searchResult);
+                searchResult.addAll(findFiles(file.getAbsolutePath()));
             }
         }
+        return searchResult;
     }
 
     public static void main(String[] args) {
         A8FindAllFilesSpecDir findAllFilesSpecDir = new A8FindAllFilesSpecDir();
         String path = "/home/andrey/Загрузки/muSolutionTask/src/main/java/";
-        List<String> fileList = new ArrayList<>();
-        findAllFilesSpecDir.findFiles (path, fileList);
+        List<String> fileList;
+        fileList = findAllFilesSpecDir.findFiles (path);
         logger.debug(fileList);
     }
 }
